c36a60993dcc4072c823fb6809ac5eb4f3c7c8cb
Kerosin3 <keromvp@gmail.com>
2023-03-16 15:59:44 +0300

    added test no roots v2

--- homework1_unittests/src/lib/squareroot.rs
+++ homework1_unittests/src/lib/squareroot.rs
@@ -14,16 +14,19 @@ mod test1;
 /// ```
 /// use libsquareroot::try_solve_square_root;
 /// assert!(true);
-/// assert!(true);
-/// assert!(true);
 /// ```
 #[allow(unused_variables)]
+#[allow(non_snake_case)]
 pub fn try_solve_square_root(
     coeffA: f64,
     coeffB: f64,
     coeffC: f64,
 ) -> Result<Option<(f64, f64)>, ErrorSolving> {
-    todo!()
+    let discr = |a: f64, b: f64, c: f64| -> f64 { b.powf(2.0) - (4.0 * a * c) };
+    match discr(coeffA, coeffB, coeffC) {
+        rez if rez == 0.0 => Ok(None),
+        _ => todo!(),
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
