796ad416eff3802a21dc5e03f76eaf8066a5d07b
Kerosin3 <keromvp@gmail.com>
2023-04-02 22:41:18 +0300

    added pattern hw2 test v2

--- homework#2_setup_motherland/game-mechanics/src/obj_mech.rs
+++ homework#2_setup_motherland/game-mechanics/src/obj_mech.rs
@@ -1,21 +1,55 @@
 pub mod object_moving {
+    use mockall::predicate::*;
+    use mockall::*;
+    use std::fmt::Display;
+    use thiserror::Error;
     pub trait Movable {
         type Coordinates;
-        fn get_position(&self) -> Self::Coordinates;
-        fn get_velocity(&self) -> Self::Coordinates;
+        fn try_get_position(&self) -> Result<Self::Coordinates, ErrorMovindObject>;
+        fn try_get_velocity(&self) -> Result<Self::Coordinates, ErrorMovindObject>;
         fn try_set_position(&mut self, vector: Self::Coordinates) -> Result<(), ErrorMovindObject>;
     }
     pub trait MoveObj: Movable {
-        fn execute(&mut self) -> Result<(), ErrorMovindObject>
+        fn execute_move(&mut self) -> Result<(), ErrorMovindObject>
         where
-            <Self as Movable>::Coordinates: std::ops::Add<Output = Self::Coordinates>,
+            <Self as Movable>::Coordinates: std::ops::Add<Output = <Self as Movable>::Coordinates>,
         {
-            let svec = std::ops::Add::add(self.get_position(), self.get_velocity());
+            let svec = std::ops::Add::add(self.try_get_position()?, self.try_get_velocity()?);
             self.try_set_position(svec)?;
             Ok(())
         }
     }
+    pub trait Rotable {
+        type Coordinates;
+        fn get_direction_number(&self);
+        fn get_angular_velocity(&self);
+        fn try_get_direction(&self) -> Result<Directions, ErrorMovindObject>;
+        fn try_set_direction(&self) -> Result<(), ErrorMovindObject>;
+    }
+    pub trait RotateObj: Rotable {
+        fn execute_rotate(&mut self) -> Result<(), ErrorMovindObject> {
+            todo!()
+        }
+    }
+    #[derive(Copy, Clone, Debug)]
+    pub enum Directions {
+        _0,
+        _45,
+        _90,
+        _135,
+        _180,
+        _225,
+        _270,
+        _315,
+        _360,
+    }
 
+    impl std::ops::Add for Directions {
+        type Output = Self;
+        fn add(self, rhs: Self) -> Self::Output {
+            todo!()
+        }
+    }
     #[derive(Copy, Clone, Debug, Default)]
     pub struct Point2D<T> {
         pub x: T,
@@ -36,8 +70,37 @@ pub mod object_moving {
             }
         }
     }
-    #[derive(Copy, Clone, Debug)]
+    #[derive(Copy, Clone, Debug, Error, PartialEq)]
     pub enum ErrorMovindObject {
+        #[error("generic error")]
         Err,
+        #[error("error getting info")]
+        ErrGetInfo,
+    }
+    pub trait Trait1 {
+        fn func1(&self) -> Result<(), SomeError>;
+        fn func2(&self) -> Result<(), SomeError>;
+    }
+
+    pub trait Trait2: Trait1 {
+        fn funcX(&self) -> Result<(), SomeError> {
+            self.func1()?;
+            self.func2()?;
+            Ok(())
+        }
+    }
+    pub enum SomeError {
+        Err,
+    }
+    mock! {
+        TestMe {}
+        impl Trait1 for TestMe{
+            fn func1(&self) -> Result<(), SomeError>;
+            fn func2(&self) -> Result<(), SomeError>;
+
+        }
+        impl Trait2 for TestMe{
+            fn funcX(&self) -> Result<(), SomeError>;
+        }
     }
 }
--- homework#2_setup_motherland/game-mechanics/tests/test_obj.rs
+++ homework#2_setup_motherland/game-mechanics/tests/test_obj.rs
@@ -11,6 +11,8 @@ mod oject_interface_test {
     #[cfg(test)]
     mod test_moving_object {
         use super::*;
+        use mockall::predicate::*;
+        use mockall::*;
         use rstest::rstest;
         use std::convert::{From, Into};
         #[derive(Copy, Clone, Debug, Default)]
@@ -34,11 +36,15 @@ mod oject_interface_test {
         // implement base interface
         impl Movable for SpaceShip<i32> {
             type Coordinates = Point2D<i32>;
-            fn get_position(&self) -> Self::Coordinates {
-                self.coord
+            fn try_get_position(&self) -> Result<Self::Coordinates, ErrorMovindObject> {
+                let _magic_value = Point2D { x: 1, y: -1 };
+                match self.coord {
+                    p1 if ((p1.x != 1_i32) & (p1.y != -1_i32)) => Ok(self.coord),
+                    _ => Err(ErrorMovindObject::ErrGetInfo),
+                }
             }
-            fn get_velocity(&self) -> Self::Coordinates {
-                self.velocity
+            fn try_get_velocity(&self) -> Result<Self::Coordinates, ErrorMovindObject> {
+                Ok(self.velocity)
             }
             fn try_set_position(
                 &mut self,
@@ -49,38 +55,77 @@ mod oject_interface_test {
                 Ok(())
             }
         }
+        mock! {
+            Something1 {}
+            impl Movable for Something1{
+                type Coordinates = Point2D<i32>;
+                fn try_get_position(&self) -> Result<<Self as Movable>::Coordinates, ErrorMovindObject>;
+                fn try_get_velocity(&self) -> Result<<Self as Movable>::Coordinates, ErrorMovindObject>;
+                fn try_set_position(&mut self, vector: <Self as Movable>::Coordinates) -> Result<(), ErrorMovindObject>;
+                }
+            impl MoveObj for Something1 {
+                    fn execute_move(&mut self) -> Result<(), ErrorMovindObject>{
+                        try_get_position();
+                        try_get_velocity();
+                }
+            }
+        }
         impl MoveObj for SpaceShip<i32> {} // default
+                                           //----------------------------------
+                                           //----------------------------------
+        #[test]
+        fn test_error_getting_position() {
+            use mockall::predicate::*;
+            use mockall::*;
+
+            let expectd = Err(ErrorMovindObject::Err);
+
+            let mut mock_moving = Box::new(MockSomething1::new());
+            mock_moving
+                .expect_execute_move()
+                .times(1)
+                .returning(move || expectd);
+            mock_moving.execute_move().unwrap();
+        }
     }
-    #[rstest]
-    #[case((12_i32,5_i32),(-7_i32,3_i32),(5_i32,8_i32))]
-    fn it_works(
-        #[case] input_pos: (i32, i32),
-        #[case] input_vel: (i32, i32),
-        #[case] expected_pos: (i32, i32),
-    ) {
-        use test_moving_object::*;
-        let mut sh1 = SpaceShip::new();
-        let init_post = Point2D {
-            x: input_pos.0,
-            y: input_pos.1,
-        };
-        let init_vel = Point2D {
-            x: input_vel.0,
-            y: input_vel.1,
-        };
-        sh1.set_initial(init_post, init_vel);
-        assert_eq!(
-            Into::<(i32, i32)>::into(sh1.get_position()),
-            init_post.into()
-        );
-        assert_eq!(
-            Into::<(i32, i32)>::into(sh1.get_velocity()),
-            init_vel.into()
-        );
-        assert!(sh1.execute().is_ok());
-        assert_eq!(
-            Into::<(i32, i32)>::into(sh1.get_position()),
-            expected_pos.into()
-        );
+    mod testing_implement {
+        use super::test_moving_object::*;
+        use super::*;
+        use mockall::predicate::*;
+        use mockall::*;
+        use rstest::rstest;
+        use std::convert::{From, Into};
+        /*
+        #[rstest]
+        #[case((12_i32,5_i32),(-7_i32,3_i32),(5_i32,8_i32))]
+        fn it_works(
+            #[case] input_pos: (i32, i32),
+            #[case] input_vel: (i32, i32),
+            #[case] expected_pos: (i32, i32),
+        ) {
+            let mut sh1 = SpaceShip::new();
+            let init_post = Point2D {
+                x: input_pos.0,
+                y: input_pos.1,
+            };
+            let init_vel = Point2D {
+                x: input_vel.0,
+                y: input_vel.1,
+            };
+            sh1.set_initial(init_post, init_vel);
+            assert_eq!(
+                Into::<(i32, i32)>::into(sh1.try_get_position().unwrap()),
+                init_post.into()
+            );
+            assert_eq!(
+                Into::<(i32, i32)>::into(sh1.try_get_velocity().unwrap()),
+                init_vel.into()
+            );
+            assert!(sh1.execute_move().is_ok());
+            assert_eq!(
+                Into::<(i32, i32)>::into(sh1.try_get_position().unwrap()),
+                expected_pos.into()
+            );
+        }*/
     }
 }
