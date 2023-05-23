b69a76da6f7f43428975f23493d2b5e7318541a9
Kerosin3 <keromvp@gmail.com>
2023-05-23 23:09:45 +0300

    hw9v1

--- homework9_communication_broker/src/agent/bin/main.rs
+++ homework9_communication_broker/src/agent/bin/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rumqttc::{AsyncClient, MqttOptions, QoS};
+use std::error::Error;
+use std::time::Duration;
+use tokio::{task, time};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut mqttoptions = MqttOptions::new("rumqtt-async", "localhost", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(60));
+
+    let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    client
+        .subscribe("hello/rumqtt", QoS::AtMostOnce)
+        .await
+        .unwrap();
+
+    loop {
+        let notification = eventloop.poll().await.unwrap();
+        println!("Received = {:?}", notification);
+    }
+    Ok(())
 }
